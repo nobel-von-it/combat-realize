d87ea808e2c7b492b0f8c2e94653ed93bb31563d
nobel-von-it <maksimdavydenko12@gmail.com>
2024-05-27 21:17:49 +0700

    better code

--- src/main.rs
+++ src/main.rs
@@ -41,55 +41,63 @@ async fn main() -> anyhow::Result<()> {
 fn run<B: Backend>(t: &mut Terminal<B>, combat: &mut Combat) -> anyhow::Result<()> {
     loop {
         t.draw(|f| combat.draw(f))?;
-        if let Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Release {
-                continue;
-            }
-            if combat.is_fin() {
-                break;
-            } else if combat.step_info.step == Step::Player {
-                match key.code {
-                    KeyCode::Up | KeyCode::Char('w') | KeyCode::Char('k') => combat.player.up(),
-                    KeyCode::Down | KeyCode::Char('s') | KeyCode::Char('j') => combat.player.down(),
-                    KeyCode::Esc => break,
-                    KeyCode::Enter => match combat.player.get_action() {
-                        Action::Hit => {
-                            combat.hit_monster();
-                            combat.toggle_step();
-                        }
-                        Action::Defense => {
-                            // it needs to realize step system player and monster
-                            // if this action is pressed, in next step if a monster is attacking, player will get less damage
-                            // but right now this will not work
-                            combat.toggle_step();
-                        }
-                        Action::Info => {
-                            // make something in draw
-                            // show player and monster stats
-                        }
-                        Action::Run => {
-                            combat.run_away();
-                            combat.toggle_step();
-                        }
-                    },
-                    KeyCode::Tab => {
-                        // make same as Action::Info
+        if combat.is_fin() {
+            break;
+        }
+        match combat.step_info.step {
+            Step::Monster => {
+                std::thread::sleep(std::time::Duration::from_millis(
+                    rand::thread_rng().gen_range(100..=1000),
+                ));
+                match combat.monster.get_monster_action(&combat.player) {
+                    Action::Hit => {
+                        combat.hit_player();
+                        combat.toggle_step();
+                    }
+                    Action::Run => {
+                        combat.run_away();
                     }
                     _ => {}
+                };
+            }
+            Step::Player => {
+                if let Event::Key(key) = event::read()? {
+                    if key.kind == KeyEventKind::Release {
+                        continue;
+                    }
+                    match key.code {
+                        KeyCode::Up | KeyCode::Char('w') | KeyCode::Char('k') => combat.player.up(),
+                        KeyCode::Down | KeyCode::Char('s') | KeyCode::Char('j') => {
+                            combat.player.down()
+                        }
+                        KeyCode::Esc => break,
+                        KeyCode::Enter => match combat.player.get_action() {
+                            Action::Hit => {
+                                combat.hit_monster();
+                                combat.toggle_step();
+                            }
+                            Action::Defense => {
+                                // it needs to realize step system player and monster
+                                // if this action is pressed, in next step if a monster is attacking, player will get less damage
+                                // but right now this will not work
+                                combat.toggle_step();
+                            }
+                            Action::Info => {
+                                // make something in draw
+                                // show player and monster stats
+                            }
+                            Action::Run => {
+                                combat.run_away();
+                                combat.toggle_step();
+                            }
+                        },
+                        KeyCode::Tab => {
+                            // make same as Action::Info
+                        }
+                        _ => {}
+                    }
                 }
             }
-        }
-        if combat.step_info.step == Step::Monster {
-            match combat.monster.get_monster_action(&combat.player) {
-                Action::Hit => {
-                    combat.hit_player();
-                    combat.toggle_step();
-                }
-                Action::Run => {
-                    combat.run_away();
-                }
-                _ => {}
-            };
         }
     }
     Ok(())
